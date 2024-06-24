c1cd11f30d60708d659e5f11090d231a37eca011
Nathan Freire <nmf2@cin.ufpe.br>
2024-06-24 19:30:18 -0300

    refactor: Update Vue version and fix component initialization in playground.ts

--- src/playground.ts
+++ src/playground.ts
@@ -1,5 +1,10 @@
-import { createApp } from "vue"
+import Vue from "vue"
 import Playground from "./GanttPlayground.vue"
 import ganttastic from "./vue-ganttastic.js"
 
-createApp(Playground).use(ganttastic).mount("#app")
+Vue.use(ganttastic)
+
+new Vue({
+  el: "#app",
+  render: (h) => h(Playground)
+})
