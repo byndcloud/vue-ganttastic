a711305fb3dc41fba1acc5f7d3ae7d25752db6e6
Nathan Freire <nmf2@cin.ufpe.br>
2024-06-24 19:32:48 -0300

    feat: Update provideConfig to return GGanttChartConfig
    The code changes in `provideConfig.ts` update the `provideConfig` function to explicitly return `GGanttChartConfig` type. This ensures type safety and improves code readability.

--- src/provider/provideConfig.ts
+++ src/provider/provideConfig.ts
@@ -1,10 +1,11 @@
 import { inject } from "vue"
 import { CONFIG_KEY } from "./symbols.js"
+import type { GGanttChartConfig } from "src/components/GGanttChart.vue"
 
 export default function provideConfig() {
   const config = inject(CONFIG_KEY)
   if (!config) {
     throw Error("Failed to inject config!")
   }
-  return config
+  return config as GGanttChartConfig
 }
